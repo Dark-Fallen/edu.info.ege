e40dedc6b473bab28568e702639f4186cd2f38c5
Dark-Fallen <57632841+Dark-Fallen@users.noreply.github.com>
2023-01-16 12:22:44 +0300

    Update ege-12.cs

--- ege-12.cs
+++ ege-12.cs
@@ -1,1 +1,10 @@
+// Для реализации функции заменить(v, w) можно использовать следующий пример
 
+public static string MyReplaceMethod(this string root, string substring1, string substring2)
+{
+  root = root.Insert(root.IndexOf(substring1), substring2);
+  root = root.Remove(root.IndexOf(substring1), substring1.Length);
+  return root;
+}
+
+// Для реализации функции нашлось(v) достаточно использовать метод Contains()
