25e0b620d6d3f89334af7ae896ec20d5cdb47cc0
Dark-Fallen <57632841+Dark-Fallen@users.noreply.github.com>
2023-01-16 12:25:00 +0300

    Update ege-12.cs

--- ege-12.cs
+++ ege-12.cs
@@ -8,3 +8,13 @@ public static string MyReplaceMethod(this string root, string substring1, string
 }
 
 // Для реализации функции нашлось(v) достаточно использовать метод Contains()
+
+/////////////////////////////// Пример использования
+static void Main(string[] args)
+{
+  string val = new string('2', 10);     // val = "2222222222"
+  val = val.MyReplaceMethod("22", "1"); // val = "122222222"
+  Console.WriteLine(val);               // 122222222
+  Console.ReadKey();
+}
+/////////////////////////////// Пример использования
